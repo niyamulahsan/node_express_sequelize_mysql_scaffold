ceaf2f4903120e098933b96d194002ba81d7c5c0
niYam <niyamulahsan@gmail.com>
2024-10-20 01:51:14 +0600

    swagger title change

--- src/global/swaggerSetup.js
+++ src/global/swaggerSetup.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Node Express Scaffold",
+      title: "Node Express Sequelize MySQL Scaffold",
       version: "1.0.0",
       description: "Scaffold server end",
       contact: {
